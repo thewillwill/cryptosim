76556bf7ee051e75b8d69bea6654f790e409d014
William Brooks <brooks.william@gmail.com>
2017-11-28 17:11:16 -0800

    added handlebars views, package dependencies and completed html routes

---
+++ routes/api-routes.js
@@ -0,0 +1,26 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+
+//insert routes here
+
+
+// app.get("/api/user/:id" or something similar
+
+
+// app.post("/api/user/:id" or something similar
+
+
+
+};
