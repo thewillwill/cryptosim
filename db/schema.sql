3c7706ad2b466c1b19d2f1e6981428462fc003b6
JPauloBR <jpaulo.araras@gmail.com>
2017-11-28 22:33:57 -0800

    table coins created and populated with cryptocompare api

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE cryptosim;
+
+use cryptosim;
+
+CREATE TABLE coins
+(
+	id int NOT NULL,
+	key_id varchar(255) NOT NULL,
+	base_url varchar(255) DEFAULT 'https://www.cryptocompare.com',
+	url varchar(255),
+	image_url varchar(255),
+	name varchar(255) NOT NULL,
+	symbol varchar(255) NOT NULL,
+	coin_name varchar(255) NOT NULL,
+	full_name varchar(255) NOT NULL,
+	sort_order int NOT NULL,
+	PRIMARY KEY (id)
+);
