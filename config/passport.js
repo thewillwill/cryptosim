30de4299160665eb4bb1326050b8a6fcc694d889
David <djlowes@gmail.com>
2017-11-30 14:26:00 -0800

    passport finished

--- config/passport.js
+++ config/passport.js
@@ -4,49 +4,62 @@
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
-//Load User Model
-// var User = sequelize.import('../models/user.js');
-// User.sync();
+//Load DB & models
+var db = require('../models/index.js');
 
 //Load Auth Variables
 var configAuth = require('./auth');
 
-
 module.exports = function(passport) {
-
-  passport.use(new FacebookStrategy({
-      clientID: configAuth.facebookAuth.clientID,
-      clientSecret: configAuth.facebookAuth.clientSecret,
-      callbackURL: configAuth.facebookAuth.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-
-      //Using next tick to take advantage of async properties
-
-      // process.nextTick(function() {
-      //   User.findOne({
-      //     where: {
-      //       User.facebook_id: profile.id
-      //     }
-      //   }).then(function(err, user) {
-      //     if(err)
-      //       return done(err);
-      //     if(user)
-      //       return done(null, user);
-      //     else {
-      //       var newUser = new User();
-      //       newUser.User.facebook_id = profile.id;
-      //       newUser.User.token = accessToken;
-      //       newUser.User.name = profile.name.givenName + " " + profile.name.familyName;
-      //       newUser.User.email = profile.emails[0].value;
-      //
-      //       newUser.save(function(err) {
-      //         if(err) throw err;
-      //         return done(null, newUser);
-      //       })
-      //     }
-      //   })
-      // })
+  // used to serialize the user for the session
+  passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id).then(function(user){
+      done(null, user);
+    }).catch(function(e){
+      done(e, false);
+    });
+    });
+    passport.use(new FacebookStrategy({
+        clientID: configAuth.facebookAuth.clientID,
+        clientSecret: configAuth.facebookAuth.clientSecret,
+        callbackURL: configAuth.facebookAuth.callbackURL,
+        passReqToCallback: true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+      },
+    function(req, token, refreshToken, profile, done) {
+    // check if the user is already logged in
+     if (!req.user) {
+       db.User.findOne({ where :{ 'facebook_id' : profile.id }}).then (function (user) {
+		 if (user) { // if there is a user id already but no token (user was linked at one point and then removed)
+		 if (!user.token) {
+				user.token = token;
+				user.name  = profile.name.givenName + ' ' + profile.name.familyName;
+				user.email = profile.emails[0].value;
+				user.save().then( function() {done(null, user);}).catch (function(e) {});
+     } else {
+				done(null, user);
+		 }
+		 } else {
+		 // if there is no user, create them
+		   var newUser = db.User.build ({
+		       facebook_id: profile.id,
+		       token: token,
+		       name: profile.name.givenName + ' ' + profile.name.familyName,
+		       email: profile.emails[0].value
+		       });
+		       newUser.save().then( function() {done(null, user);}).catch (function(e) {});
+			   }
+		  });
+   } else { // user already exists and is logged in, we have to link accounts
+      var user                = req.user; // pull the user out of the session
+          user.facebook_id    = profile.id;
+          user.token          = token;
+          user.name           = profile.name.givenName + ' ' + profile.name.familyName;
+          user.email          = profile.emails[0].value;
+          user.save().then( function() {done(null, user);}).catch (function(e) {});
     }
-  ));
+  }));
 }
