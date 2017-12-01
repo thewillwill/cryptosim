279fe1e4899ef203b0dc4dafe7013621980acbfa
William Brooks <brooks.william@gmail.com>
2017-12-01 11:09:40 -0800

    got table sorter working on market page

--- public/assets/js/frontend.js
+++ public/assets/js/frontend.js
@@ -6,17 +6,26 @@ for (var i = 6; i >= 0; i--) {
     dates.push(day);
     $("#tres").append(dates);
 }
+
+
 $(document).ready(function() {
     console.log("Document Ready to Run");
+
+
+    console.log('tablesorter(1)');
+    //set the tablesorter plugin to initialise on market-table
     $("#market-table").tablesorter();
+
+    //get the currencies json object
     var queryURL = "/api/currencies";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).done(function(response) {
         var results = response;
+
+        //add rows to table
         for (var i = 0; i < results.length; i++) {
-            var newBody = $("<tbody>");
             var newRow = $("<tr>");
             var newIcon = $("<td>");
             var newSpan = $("<span>");
@@ -30,64 +39,77 @@ $(document).ready(function() {
             newName.append(results[i].coin_name);
             var newValue = $("<td>");
             newValue.append(10);
-            newBody.append(newRow);
-            newBody.append(newIcon);
-            newBody.append(newID);
-            newBody.append(newName);
-            newBody.append(newValue);
-            $("#market-table").append(newBody);
+
+            newRow.append(newIcon);
+            newRow.append(newID);
+            newRow.append(newName);
+            newRow.append(newValue);
+            $("#market-table-body").append(newRow);
+
         }
+        console.log('after for loop: ');
+        $("#market-table").trigger("update");
+        console.log('after trigger(update) ');
+
+
     });
-})
 
-if ($('#summaryChart').length > 0) {
-    var ctx = document.getElementById("summaryChart").getContext('2d');
-    var summaryChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: dates,
-            datasets: [{
-                label: 'Profit and Loss Summary',
-                data: netWorths,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            responsive: true,
-            scales: {
-                yAxes: [{
-                    display: true,
-                    ticks: {
-                        beginAtZero: true,
-                        steps: 2000
 
-                    }
+    if ($('#summaryChart').length > 0) {
+        var ctx = document.getElementById("summaryChart").getContext('2d');
+        var summaryChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: 'Profit and Loss Summary',
+                    data: netWorths,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
                 }]
-            }
-        }
-    });
-}
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        display: true,
+                        ticks: {
+                            beginAtZero: true,
+                            steps: 2000
 
-// Cover Page Jquery
-//scroll down from top arrow
-$("#arrow").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#feature-1").offset().top
-    }, 2000);
-})
+                        }
+                    }]
+                }
+            }
+        });
+    }
+
+
+
+    console.log('$', "#portfolio-table");
+    $("#portfolio-table").tablesorter();
+
+    // Cover Page Jquery
+    //scroll down from top arrow
+    $("#arrow").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#feature-1").offset().top
+        }, 2000);
+    })
+
+});
