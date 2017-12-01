1a4f604c351ffd29cb5936a87ed834294a82e38a
JPauloBR <jpaulo.araras@gmail.com>
2017-11-30 17:47:30 -0800

    jp version of api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,6 +5,9 @@
 // Dependencies
 // =============================================================
 
+global.fetch = require('node-fetch');
+const cc = require('cryptocompare');
+
 // Requiring our models
 var db = require("../models");
 var Sequelize = require('sequelize');
@@ -27,6 +30,17 @@ app.get("/api/user/:id", function(req, res) {
 //New User
 app.post("/api/user/new", function(req, res) {
 	db.User.create(req.body).then(function(dbPost) {
+		console.log(dbPost.id);
+		var newPort = 
+		{
+			UserId: dbPost.id,
+			currency: "USD",
+			amount: 50000,
+			expired: 0
+		};
+		db.Portfolio.create(newPort).then(function(dbPort) {
+			return true;
+		});
 		res.json(dbPost);
 	})
 });
@@ -39,57 +53,78 @@ app.get("/api/currencies", function(req,res) {
 	})
 })
 
+// PUT route for updating User
+app.put("/api/user", function(req, res) {
+  db.User.update(
+    req.body,
+    {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
+});
+
+//get currency actual value
+app.get("/api/currencies/:symbol", function(req, res) {
+	var symbol = req.params.symbol.toUpperCase();
+	cc.priceMulti([symbol], ['USD'])
+	.then(prices => {
+	  console.log(prices)
+	  res.json(prices);
+	})
+	.catch(console.error)
+})
+
+//get currency historical value
+app.get("/api/currencies/:symbol/:date", function(req, res) {
+	var symbol = req.params.symbol.toUpperCase();
+	cc.priceHistorical(symbol, ['USD'], new Date(req.params.date))
+	.then(prices => {
+	  console.log(prices)
+	  res.json(prices);
+	})
+	.catch(console.error)
+})
+
+//get currency value for the last 30 days
+app.get("/api/curr-hist-day/:symbol", function(req, res) {
+	var symbol = req.params.symbol.toUpperCase();
+	cc.histoDay(symbol, ['USD'])
+	.then(prices => {
+	  console.log(prices)
+	  res.json(prices);
+	})
+	.catch(console.error)
+})
+
+//get currency value for the last week hourly
+app.get("/api/curr-hist-hour/:symbol", function(req, res) {
+	var symbol = req.params.symbol.toUpperCase();
+	cc.histoHour(symbol, ['USD'])
+	.then(prices => {
+	  console.log(prices)
+	  res.json(prices);
+	})
+	.catch(console.error)
+})
+
+//get currency full price
+app.get("/api/curr-price-full/:symbol", function(req, res) {
+	var symbol = req.params.symbol.toUpperCase();
+	cc.priceFull(symbol, ['USD'])
+	.then(prices => {
+	  console.log(prices)
+	  res.json(prices);
+	})
+	.catch(console.error)
+})
+
 
 
 };
 
-// var express = require("express");
-
-// // Import the model (burger.js) to use its database functions.
-// // Requiring our models
-// var db = require("../models");
-
-// var Sequelize = require('sequelize');
-// require('sequelize-values')(Sequelize);
-
-
-// var router = express.Router();
-
-
-// // Create all our routes and set up logic within those routes where required.
-// router.get("/", function(req, res) {
-//   // burger.selectAll(function(data) {
-//   //   var hbsObject = {
-//   //     burgers: data
-//   //   };
-//   //   res.render("index", hbsObject);
-//   // });
-
-//   db.Burger.findAll({}).then(function(dbPost) {
-//     // res.json(dbPost);
-//     console.log(Sequelize.getValues(dbPost));
-//     //var newBurger = dbPost.toJSON();
-//      var newBurger = {
-//       burgers: Sequelize.getValues(dbPost)
-//      };
-//     // console.log(newBurger);
-//     res.render("index", newBurger);
-//   });
-// });
-
-// router.post("/api/burgers", function(req, res) {
-//   // burger.insertOne([
-//   //   "burger_name", "devoured"
-//   // ], [
-//   //   req.body.burger_name, req.body.devoured
-//   // ], function(result) {
-//   //   // Send back the ID of the new quote
-//   //   res.json({ id: result.insertId });
-//   // });
-//   db.Burger.create(req.body).then(function(dbPost) {
-//     res.json(dbPost);
-//   })
-// });
 
 // router.put("/api/burgers/:id", function(req, res) {
 //   // var condition = "id = " + req.params.id;
